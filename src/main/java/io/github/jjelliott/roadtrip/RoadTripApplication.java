e9c173ce3fca4afcb355ad35336af11a4684efdc
Jesse Elliott <jesseelliott@outlook.com>
2024-07-07 14:28:17 -0400

    Add initializr app

---
+++ src/main/java/io/github/jjelliott/roadtrip/RoadTripApplication.java
@@ -0,0 +1,13 @@
+package io.github.jjelliott.roadtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jjelliott/roadtrip/RoadTripApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jjelliott.roadtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
